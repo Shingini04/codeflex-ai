707f2941c3d15b26c0a7b7e9aa5342eb17131804
Shingini04 <lahirishingini@gmail.com>
2025-06-08 21:57:58 +0530

    Initial commit

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://peaceful-baboon-38.clerk.accounts.dev/",
+      domain: "https://excited-marten-46.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
